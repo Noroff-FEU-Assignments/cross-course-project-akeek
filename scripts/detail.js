30817df0d7b11e32f838bc98737a1018a973c278
Åke Ek <svein.ek@stud.noroff.no>
2022-04-23 16:43:27 +0200

    Add java

---
+++ scripts/detail.js
@@ -0,0 +1,35 @@
+const detailContainer = document.querySelector(".details");
+
+const queryString = document.location.search;
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+
+
+const url = "https://www.bolf.one/wp-json/wc/store/products/" + id;
+
+
+async function fetchProduct(){
+    try{
+        const response = await fetch(url);
+        const results = await response.json();
+
+        createHtml(results)
+    }
+
+    catch(error){
+        console.log(error)
+    }
+}
+
+
+function createHtml(products) {
+    console.log(products)
+    detailContainer.innerHTML = 
+    `<h1>${products.name}</h1>
+        <img src="${products.images[0].src}" alt="${products.name}">
+        <div>${products.short_description}</div>`
+}
+
+fetchProduct();
---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+const url = "https://www.bolf.one/wp-json/wc/store/products";
+const productContainer = document.querySelector(".products");
+
+
+async function getProducts(){
+    try{
+        const response = await fetch(url);
+        const getResults = await response.json();
+        createHtml(getResults);
+    }
+
+    catch(error){
+        console.log(error)
+    }
+}
+
+
+
+function createHtml(products){
+    products.forEach(function(product){
+        console.log(product)
+        productContainer.innerHTML += 
+        `<a href="details.html?id=${product.id}" class="product">
+            <h2>${product.name}</h2>
+            <img src="${product.images[0].src}" alt="${product.name}">
+        </a>`;
+    })
+}
+
+getProducts();
